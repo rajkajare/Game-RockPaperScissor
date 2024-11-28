f6c4fa25c40dbdc2cc3868d97d008d04c7bb13f2
Raj Kajare <rajkajare21@gmail.com>
2024-11-28 19:35:16 +0530

    version-6

--- js/general.js
+++ js/general.js
@@ -6,6 +6,37 @@ if (!score) {
         .innerHTML = `Wins: ${score.wins} &nbsp; Loses: ${score.loses} &nbsp; Tie: ${score.tie}`;
 }
 
+
+
+
+
+document.querySelector('.js-rock').addEventListener('click', ()=>{
+    if (isAutoPlaying) {
+        autoplay();
+    } else if (isShowingPopup) {
+        popup();
+    }
+    playGame('Rock');
+});
+
+document.querySelector('.js-paper').addEventListener('click', ()=>{
+    if (isAutoPlaying) {
+        autoplay();
+    } else if (isShowingPopup) {
+        popup();
+    }
+    playGame('Paper');
+});
+
+document.querySelector('.js-scissor').addEventListener('click', ()=>{
+    if (isAutoPlaying) {
+        autoplay();
+    } else if (isShowingPopup) {
+        popup();
+    }
+    playGame('Scissor');
+});
+
 function pickComputerMove() {
     const value = Math.random();
     let computerMove = '';
@@ -68,22 +99,129 @@ function playGame(yourMove) {
         .innerHTML = `Wins: ${score.wins} &nbsp; Loses: ${score.loses} &nbsp; Tie: ${score.tie}`;
 }
 
+
+
+
+
+
+document.querySelector('.js-reset').addEventListener('click', ()=>{
+    if (isAutoPlaying) {
+        autoplay();
+    }
+    popup();
+})
+
+let isShowingPopup = false;
+function popup() {
+    if (! isShowingPopup) {
+        document.querySelector('.js-popup').classList.add('popup');
+        document.querySelector('.js-popup')
+            .innerHTML = `
+                <p class="popup-text">Are you sure you want to reset the score ?</p>
+                <button class="js-yes popup-button">Yes</button> 
+                <button class="js-no popup-button">No</button>
+            `;
+        document.querySelector('.js-yes').addEventListener('click', ()=>{
+            reset();
+            popup('hide');
+        });
+        document.querySelector('.js-no').addEventListener('click', ()=>{
+            popup('hide');
+        });
+        isShowingPopup = true;
+    } else {
+        document.querySelector('.js-popup').classList.remove('popup');
+        document.querySelector('.js-popup')
+            .innerHTML = ``;
+        isShowingPopup = false;
+    }
+}
+
+function reset() {
+    score.wins = 0;
+    score.loses = 0;
+    score.tie = 0;
+    localStorage.removeItem('score');
+    document.querySelector('.js-result')
+        .innerHTML = '';
+    document.querySelector('.js-scene')
+        .innerHTML = '';
+    document.querySelector('.js-score')
+            .innerHTML = `Wins: ${score.wins} &nbsp; Loses: ${score.loses} &nbsp; Tie: ${score.tie}`;
+}
+
+
+
+
+
 let autoplayButton = document.querySelector('.js-autoplay');
 let isAutoPlaying = false;
+autoplayButton.addEventListener('click', ()=>{
+    autoplay();
+});
 let intervalId;
 
 function autoplay() {
+    if (isShowingPopup) {
+        popup();
+    };
     if (! isAutoPlaying) {
-        autoplayButton.innerText = "Stop Autoplay"
-        intervalId = setInterval(function() {
+        autoplayButton.innerText = "Stop Playing";
+        intervalId = setInterval(() => {
                         let yourMove = pickComputerMove();
                         playGame(yourMove);
                     }, 1000);
         isAutoPlaying = true;
     } else {
-        autoplayButton.innerText = "Autoplay"
+        autoplayButton.innerText = "Autoplay";
         clearInterval(intervalId);
         isAutoPlaying = false;
-    }
-    
+    }    
 }
+
+
+
+
+
+document.body.addEventListener('keydown', (event)=>{
+    if (event.key === 'r' ) {
+        if (isAutoPlaying) {
+            autoplay();
+        } else if (isShowingPopup) {
+            popup();
+        }
+        playGame('Rock');
+    } 
+    if (event.key === 'p' ) {
+        if (isAutoPlaying) {
+            autoplay();
+        } else if (isShowingPopup) {
+            popup();
+        }
+        playGame('Paper');
+    } 
+    if (event.key === 's' ) {
+        if (isAutoPlaying) {
+            autoplay();
+        } else if (isShowingPopup) {
+            popup();
+        }
+        playGame('Scissor');
+    } 
+    if (event.key === 'Backspace' ) {
+        if (isAutoPlaying) {
+            autoplay();
+        }
+        popup();
+    } 
+    if (event.key === 'a') {
+        autoplay();
+    }
+    if (event.key === 'y') {
+        reset();
+        popup();
+    }
+    if (event.key === 'n') {
+        popup();
+    }
+})
